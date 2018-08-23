6a5a5234d88de355a3ab2ee54ae7866e0d337fa8
Matthew Conlen <mc@mathisonian.com>
2018-08-23 10:26:14 -0700

    cleanup, remove build files, update idyll version

--- node_modules/idyll-layouts/README.md
+++
@@ -1,4 +0,0 @@
-# idyll-layouts
-Layouts that ship by default in new idyll projects.
-
-*See http://idyll-lang.org for complete documentation.*
--- node_modules/idyll-themes/src/idyll/index.js
+++
@@ -1,9 +0,0 @@
-import createStyles from './styles';
-
-const config = {
-};
-
-export default {
-  ...config,
-  styles: createStyles(config)
-};
