c8005e2055895e7eaca187a9cb3b6063830ab3f7
Megan Vo <meganvo@cs.washington.edu>
2019-01-10 13:46:30 -0800

    add audio context that requires click

--- components/ThreeFourDemo.js
+++ components/ThreeFourDemo.js
@@ -1,6 +1,6 @@
-const React = require('react');
-import { VictoryAnimation } from 'victory';
-import CircleGraphic from './CircleGraphic.js';
+const React = require("react");
+const StartAudioContext = require("startaudiocontext");
+import CircleGraphic from "./CircleGraphic.js";
 
 var Tone;
 var sampler;
@@ -20,45 +20,55 @@ class ThreeFourDemo extends React.Component {
   }
 
   componentDidMount() {
-    Tone = require('tone');
+    Tone = require("tone");
+    var context = new AudioContext();
     // creates it once to avoid overlapping synths
     sampler = new Tone.Sampler({
-      "C4": "static/sounds/bassdrum4.wav",
-      "E4": "static/sounds/silence.wav",
-      "D4": "static/sounds/hihat3.wav"
+      C4: "static/sounds/bassdrum4.wav",
+      E4: "static/sounds/silence.wav",
+      D4: "static/sounds/hihat3.wav"
     }).toMaster();
 
     // To avoid overlapping patterns, declare here
     // Allows stop and start to end where it left off
-    pattern = new Tone.Sequence(function (time, note) {
-      this.animateCircles(note, time);
-      sampler.triggerAttackRelease(note, .25);
-    }.bind(this), ["C4", "E4", "D4", "E4", "D4", "E4"], "4n");
+    pattern = new Tone.Sequence(
+      function(time, note) {
+        this.animateCircles(note, time);
+        sampler.triggerAttackRelease(note, 0.25);
+      }.bind(this),
+      ["C4", "E4", "D4", "E4", "D4", "E4"],
+      "4n"
+    );
 
     // Make sure it is mounted before loading up
     // sampler
     this.setState({ mounted: true });
+    StartAudioContext(Tone.context, "test-three-four");
   }
 
   // Animates the circle in sync with the current
   // note being played
   animateCircles(note, time) {
-    Tone.Draw.schedule(function () {
-      this.props.updateProps({
-        beatNum: ((this.props.beatNum) % 6) + 1
-      })
+    Tone.Draw.schedule(
+      function() {
+        this.props.updateProps({
+          beatNum: (this.props.beatNum % 6) + 1
+        });
 
-      this.setState({ onBeat: this.state.onBeat + 1 });
-      this.setState({ rotation: "rotate(" + this.state.degrees + "  200 150)" });
-      this.setState({ degrees: this.state.degrees + 60 });
-    }.bind(this), time);
+        this.setState({ onBeat: this.state.onBeat + 1 });
+        this.setState({
+          rotation: "rotate(" + this.state.degrees + "  200 150)"
+        });
+        this.setState({ degrees: this.state.degrees + 60 });
+      }.bind(this),
+      time
+    );
   }
 
   // Toggles play on and off and creates a synth
-  // to be played. Changes the button text to 
+  // to be played. Changes the button text to
   // on/off
   playAudio() {
-
     // Play the audio when loaded and clicked
     if (Tone.Transport.state === "stopped") {
       Tone.Transport.bpm.value = 120;
@@ -114,17 +124,39 @@ class ThreeFourDemo extends React.Component {
   }
 
   render() {
-    const { steps, beatNum, hasError, idyll, updateProps, ...props } = this.props;
+    const {
+      steps,
+      beatNum,
+      hasError,
+      idyll,
+      updateProps,
+      ...props
+    } = this.props;
     var beat = this.state.onBeat;
     return [
-      <div className="hoverable" onMouseEnter={this.playAudio.bind(this)} onMouseLeave={this.turnOff.bind(this)}>
-        <CircleGraphic numCircles={3} placement={[90, 210, 330]} opacity={this.state.opacity}
-          miniOpacity={[beat % 6 === 1 ? 0.9 : 0.5, beat % 6 === 3 ? 0.9 : 0.5, beat % 6 === 5 ? 0.9 : 0.5]}
-          fill={["#FF851B", "#087E8B", "#087E8B"]} rotation={this.state.rotation}
+      <div
+        id="test-three-four"
+        className="hoverable"
+        onMouseEnter={this.playAudio.bind(this)}
+        onMouseLeave={this.turnOff.bind(this)}
+      >
+        <CircleGraphic
+          numCircles={3}
+          placement={[90, 210, 330]}
+          opacity={this.state.opacity}
+          miniOpacity={[
+            beat % 6 === 1 ? 0.9 : 0.5,
+            beat % 6 === 3 ? 0.9 : 0.5,
+            beat % 6 === 5 ? 0.9 : 0.5
+          ]}
+          fill={["#FF851B", "#087E8B", "#087E8B"]}
+          rotation={this.state.rotation}
           showText={steps === 3 || steps === 4}
-          name="ThreeFour" label={steps > 1 ? "3/4" : ""} />
+          name="ThreeFour"
+          label={steps > 1 ? "3/4" : ""}
+        />
       </div>
-    ]
+    ];
   }
 }
 
